22bdafeccf9a4c98ec7338241c68ea4aa0b511ca
douhaoi <douhaoi@126.com>
2024-07-11 18:33:28 +0800

    完善左侧笔记列表效果

--- components/SidebarNoteList.js
+++ components/SidebarNoteList.js
@@ -1,6 +1,10 @@
 import SidebarNoteItem from '@/components/SidebarNoteItem';
+import { getAllNotes } from '@/lib/redis';
 
-export default async function NoteList({ notes }) {
+export default async function NoteList() {
+  const sleep = ms => new Promise(r => setTimeout(r, ms));
+  await sleep(10000);
+  const notes = await getAllNotes()
 
   const arr = Object.entries(notes);
 
